c6aaa4f0bec1cf2f58559baa5ff4eecbc6b768f3
xinyun <xinyun.fang@ucdconnect.ie>
2022-01-02 14:17:51 +0000

    change: human

--- Project1/TextureScreen.cpp
+++ Project1/TextureScreen.cpp
@@ -7,13 +7,6 @@ GLuint TextureScreen::generateObjectBuffer() {
 	// Buffer will contain an array of vertices 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	//// After binding, we now fill our object with data, everything in "Vertices" goes to the GPU
-	//glBufferData(GL_ARRAY_BUFFER, numVertices * 5 * sizeof(GLfloat), NULL, GL_STATIC_DRAW);
-	//// if you have more data besides vertices (e.g., vertex colours or normals), use glBufferSubData to tell the buffer when the vertices array ends and when the colors start
-	//glBufferSubData(GL_ARRAY_BUFFER, 0, numVertices * 5 * sizeof(GLfloat), vertices);
-	//glBufferSubData(GL_ARRAY_BUFFER, numVertices * 3 * sizeof(GLfloat), numVertices * 4 * sizeof(GLfloat), colors);
-	//glBufferSubData(GL_ARRAY_BUFFER, numVertices * 7 * sizeof(GLfloat), numVertices * 2 * sizeof(GLfloat), texture);
 	return VBO;
 }
 
@@ -21,16 +14,13 @@ void TextureScreen::linkCurrentBuffertoShader(GLuint shaderProgramID) {
 	// find the location of the variables that we will be using in the shader program
 	GLuint positionID = glGetAttribLocation(shaderProgramID, "position");
 	GLuint normalID = glGetAttribLocation(shaderProgramID, "normal");
-	GLuint textureID = glGetAttribLocation(shaderProgramID, "texCoord");
 	// Have to enable this
 
 	glEnableVertexAttribArray(positionID);
 	// Tell it where to find the position data in the currently active buffer (at index positionID)
-	glVertexAttribPointer(positionID, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(positionID, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	// Similarly, for the color data.
 	glEnableVertexAttribArray(normalID);
-	glVertexAttribPointer(normalID, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(textureID);
-	glVertexAttribPointer(textureID, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glVertexAttribPointer(normalID, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 }
 #pragma endregion VBO_FUNCTIONS
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -42,8 +42,8 @@ Shader* cubeShader;
 Shader* textureShader;
 Shader* testShader;
 Mesh *humanMesh;
-Mesh *ground;
-Mesh *house;
+Mesh *groundMesh;
+Mesh *houseMesh;
 Aim *aim;
 
 Texture* grassTexture;
@@ -52,7 +52,7 @@ Texture* red;
 Texture* sight;
 
 Game* game;
-TextureScreen* test;
+TextureScreen* human;
 
 GLfloat transform_x = 0.0f;
 GLfloat transform_y = 0.0f;
@@ -74,6 +74,10 @@ int viewstate = 0;
 int enemystate = 0;
 int enemystate2 = 0;
 
+GLfloat greenColor[3] = {0.0f, 0.55f, 0.0f};
+GLfloat bodyColor[3] = { 0.9f, 0.88f, 0.62f };
+float walk = 0;
+
 
 
 
@@ -85,33 +89,6 @@ void display() {
 	glClearColor(0.53f, 0.81f, 0.92f, 0.5f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glUseProgram(cubeShader->ID);
-	//Declare your uniform variables that will be used in your shader
-	int matrix_location = glGetUniformLocation(cubeShader->ID, "model");
-	int view_mat_location = glGetUniformLocation(cubeShader->ID, "view");
-	int proj_mat_location = glGetUniformLocation(cubeShader->ID, "proj");
-
-	mat4 view = identity_mat4();
-	view = translate(view, vec3(0.0f, 0.0f, -3.0f));
-	view = rotate_y_deg(view, view_rotate_x);
-	view = rotate_x_deg(view, view_rotate_y);
-	mat4 persp_proj = perspective(45.0f, (float)width / (float)height, 0.1f, 100.0f);
-	mat4 model = identity_mat4();
-	model = rotate_y_deg(model, -55.0f);
-
-	glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, persp_proj.m);
-	glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, view.m);
-	glUniformMatrix4fv(matrix_location, 1, GL_FALSE, model.m);
-
-	glBindBuffer(GL_ARRAY_BUFFER, test->VBO);
-	test->linkCurrentBuffertoShader(cubeShader->ID);
-	red->Bind(GL_TEXTURE0);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
-
-
-
 	if (viewstate == 1) {
 		glUseProgram(aimShader->ID);
 		glBindBuffer(GL_ARRAY_BUFFER, aim->VBO);
@@ -121,113 +98,240 @@ void display() {
 	}
 
 
-	//glUseProgram(meshShader->ID);
+	// Root of the Hierarchy
+	mat4 view = identity_mat4();
+	view = rotate_y_deg(view, view_rotate_x);
+	view = rotate_x_deg(view, view_rotate_y);
+	view = rotate_y_deg(view, 180.0f);
+	view = rotate_x_deg(view, 10.0f);
+	view = translate(view, vec3(0.0, 0.0, -10.0f));
+	if (viewstate == 1) {
+		view = translate(view, vec3(0.0, 0.0, 10.18f));
+		view = rotate_x_deg(view, -10.0f);
+	}
+	
+	mat4 persp_proj = perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
+	mat4 model = identity_mat4();
+	model = translate(model, vec3(0.0f, -10.0f, 0.0f));
+	model = rotate_y_deg(model, rotate_x);
+	
+
+
+	glUseProgram(cubeShader->ID);
 	//Declare your uniform variables that will be used in your shader
-	//int matrix_location = glGetUniformLocation(meshShader->ID, "model");
-	//int view_mat_location = glGetUniformLocation(meshShader->ID, "view");
-	//int proj_mat_location = glGetUniformLocation(meshShader->ID, "proj");
+	int cube_matrix_location = glGetUniformLocation(cubeShader->ID, "model");
+	int cube_view_mat_location = glGetUniformLocation(cubeShader->ID, "view");
+	int cube_proj_mat_location = glGetUniformLocation(cubeShader->ID, "proj");
+	int color = glGetUniformLocation(cubeShader->ID, "ourColor");
+
+	mat4 humanModel = identity_mat4();
+	humanModel = translate(humanModel, vec3(0.0f, 0.0f, -3.0f));
+	humanModel = rotate_y_deg(humanModel, -rotate_x);
+	humanModel = translate(humanModel, vec3(0.0f, 8.0f, 0.0f));
+	humanModel = scale(humanModel, vec3(1.1f, 1.2f, 1.1f));
+	humanModel = model * humanModel;
+
+	mat4 bodyModel = identity_mat4();
+	bodyModel = scale(bodyModel, vec3(0.6f, 0.85f, 0.35f));
+	bodyModel = humanModel * bodyModel;
+
+	glUniformMatrix4fv(cube_proj_mat_location, 1, GL_FALSE, persp_proj.m);
+	glUniformMatrix4fv(cube_view_mat_location, 1, GL_FALSE, view.m);
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, bodyModel.m);
+	glUniform3fv(color, 1, greenColor);
+
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	mat4 headModel = identity_mat4();
+	headModel = translate(headModel, vec3(0.0f, 1.2f, 0.0f));
+	headModel = scale(headModel, vec3(0.6f, 0.6f, 0.6f));
+	headModel = humanModel * headModel;
+
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, headModel.m);
+	glUniform3fv(color, 1, bodyColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
-	//// Root of the Hierarchy
-	//mat4 view = identity_mat4();
-	//
-	//view = rotate_y_deg(view, 180.0f);
-	//view = rotate_x_deg(view, 10.0f);
-	//view = translate(view, vec3(0.0, 0.0, -10.0f));
-	//if (viewstate == 1) {
-	//	view = translate(view, vec3(0.0, 0.0, 10.18f));
-	//	view = rotate_x_deg(view, -10.0f);
-	//}
-	//
-	//mat4 persp_proj = perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
-	//mat4 model = identity_mat4();
-	//
-	//model = translate(model, vec3(0.0f, -10.0f, 0.0f));
-	//model = rotate_y_deg(model, rotate_x);
-	//mat4 humanGround = identity_mat4();
-	//humanGround = rotate_y_deg(humanGround, -rotate_x);
-	//humanGround = translate(humanGround, vec3(0.0f, 30.0f, 0.0f));
-	//humanGround = scale(humanGround, vec3(0.25f, 0.25f, 0.25f));
-	//
-	//humanGround = model * humanGround;
+	mat4 leftShoulderModel = identity_mat4();
+	leftShoulderModel = rotate_x_deg(leftShoulderModel, 20 * sin(walk));
+	leftShoulderModel = translate(leftShoulderModel, vec3(1.5f, 3.5f, 0.0f));
+	leftShoulderModel = scale(leftShoulderModel, vec3(0.3f, 0.1f, 0.3f));
+	leftShoulderModel = humanModel * leftShoulderModel;
 
-	//// update uniforms & draw
-	//glUniformMatrix4fv(proj_mat_location, 1, GL_FALSE, persp_proj.m);
-	//glUniformMatrix4fv(view_mat_location, 1, GL_FALSE, view.m);
-	//glUniformMatrix4fv(matrix_location, 1, GL_FALSE, humanGround.m);
-	//
-	//humanMesh->linkCurrentBuffertoShader(meshShader->ID);
-	//glDrawArrays(GL_TRIANGLES, 0, humanMesh->mesh_data.mPointCount);
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, leftShoulderModel.m);
+	glUniform3fv(color, 1, greenColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
-	//glUseProgram(textureShader->ID);
-	////Declare your uniform variables that will be used in your shader
-	//int texture_matrix_location = glGetUniformLocation(textureShader->ID, "model");
-	//int texture_view_mat_location = glGetUniformLocation(textureShader->ID, "view");
-	//int texture_proj_mat_location = glGetUniformLocation(textureShader->ID, "proj");
 
 
-	//// Set up the child matrix
-	//mat4 modelGround = identity_mat4();
-	//modelGround = translate(modelGround, vec3(0.0f, -0.19f, 0.0f));
-	//modelGround = scale(modelGround, vec3(100.0f, 10.0f, 100.0f));
-	//modelGround = translate(modelGround, vec3(transform_x, 0.0f, transform_y));
-	// 
-	//// Apply the root matrix to the child matrix
-	//modelGround = model * modelGround;
-	//// Update the appropriate uniform and draw the mesh again
-	//glUniformMatrix4fv(texture_proj_mat_location, 1, GL_FALSE, persp_proj.m);
-	//glUniformMatrix4fv(texture_view_mat_location, 1, GL_FALSE, view.m);
-	//glUniformMatrix4fv(texture_matrix_location, 1, GL_FALSE, modelGround.m);
-	//ground->linkCurrentBuffertoShader(textureShader->ID);
-	//grass->Bind(GL_TEXTURE0);
-	//glDrawArrays(GL_TRIANGLES, 0, ground->mesh_data.mPointCount);
-	//
-	//
-	//mat4 modelBarrier = identity_mat4();
-	//modelBarrier = scale(modelBarrier, vec3(0.01f, 0.1f, 0.01f));
-	//modelBarrier = scale(modelBarrier, vec3(2.0f, 2.0f, 2.0f));
-	//modelBarrier = translate(modelBarrier, vec3(0.0f, 1.0f, 0.3f));
+	mat4 leftArmModel = identity_mat4();
+	leftArmModel = translate(leftArmModel, vec3(0.0f, -0.5f, 0.0f));
+	leftArmModel = scale(leftArmModel, vec3(1.0f, 5.5f, 1.0f));
+	leftArmModel = leftShoulderModel * leftArmModel;
 
-	//modelBarrier = modelGround * modelBarrier;
-
-	//// Update the appropriate uniform and draw the mesh again
-	//glUniformMatrix4fv(matrix_location, 1, GL_FALSE, modelBarrier.m);
-	//white->Bind(GL_TEXTURE0);
-	//house->linkCurrentBuffertoShader(textureShader->ID);
-	//glDrawArrays(GL_TRIANGLES, 0, house->mesh_data.mPointCount);
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, leftArmModel.m);
+	glUniform3fv(color, 1, bodyColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
-	////enemy_y = enemy_y - 0.01;
-	//mat4 modelEnemies = identity_mat4();
-	//modelEnemies = translate(modelEnemies, vec3(enemy_x, 0.0f, enemy_y));
-	//modelEnemies = scale(modelEnemies, vec3(0.01f, 0.1f, 0.01f));
-	//modelEnemies = scale(modelEnemies, vec3(0.25f, 0.25f, 0.25f));
-	//modelEnemies = translate(modelEnemies, vec3(0.0f, 0.935f, 0.0f));
-	//modelEnemies = modelGround * modelEnemies;
-	//glUniformMatrix4fv(matrix_location, 1, GL_FALSE, modelEnemies.m);
-	//if (enemystate == 0) {
-	//	red->Bind(GL_TEXTURE0);
-	//	humanMesh->linkCurrentBuffertoShader(meshShader->ID);
-	//	glDrawArrays(GL_TRIANGLES, 0, humanMesh->mesh_data.mPointCount);
-	//}
 
 
-	//modelEnemies = identity_mat4();
-	//if (enemy_y2 > -30) {
-	//	enemy_y2 = enemy_y2 - 0.01;
-	//}
-	//modelEnemies = translate(modelEnemies, vec3(enemy_x2, 0.0f, enemy_y2));
-	//modelEnemies = scale(modelEnemies, vec3(0.01f, 0.1f, 0.01f));
-	//modelEnemies = scale(modelEnemies, vec3(0.25f, 0.25f, 0.25f));
-	//modelEnemies = translate(modelEnemies, vec3(0.0f, 0.935f, 0.0f));
-	//modelEnemies = modelGround * modelEnemies;
-	//glUniformMatrix4fv(matrix_location, 1, GL_FALSE, modelEnemies.m);
-	//if (enemystate2 == 0) {
-	//	red->Bind(GL_TEXTURE0);
-	//	humanMesh->linkCurrentBuffertoShader(meshShader->ID);
-	//	glDrawArrays(GL_TRIANGLES, 0, humanMesh->mesh_data.mPointCount);
-	//}
+	mat4 rightShoulderModel = identity_mat4();
+	rightShoulderModel = rotate_x_deg(rightShoulderModel, -20 * sin(walk));
+	rightShoulderModel = translate(rightShoulderModel, vec3(-1.5f, 3.5f, 0.0f));
+	rightShoulderModel = scale(rightShoulderModel, vec3(0.3f, 0.1f, 0.3f));
+	rightShoulderModel = humanModel * rightShoulderModel;
+
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, rightShoulderModel.m);
+	glUniform3fv(color, 1, greenColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+	mat4 rightArmModel = identity_mat4();
+	rightArmModel = translate(rightArmModel, vec3(0.0f, -0.5f, 0.0f));
+	rightArmModel = scale(rightArmModel, vec3(1.0f, 5.5f, 1.0f));
+	rightArmModel = rightShoulderModel * rightArmModel;
+
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, rightArmModel.m);
+	glUniform3fv(color, 1, bodyColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
+	mat4 leftBottomModel = identity_mat4();
+	leftBottomModel = rotate_x_deg(leftBottomModel, -20 * sin(walk));
+	leftBottomModel = translate(leftBottomModel, vec3(0.5f, -2.5f, 0.0f));
+	leftBottomModel = scale(leftBottomModel, vec3(0.3f, 0.2f, 0.3f));
+	leftBottomModel = humanModel * leftBottomModel;
+
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, leftBottomModel.m);
+	glUniform3fv(color, 1, greenColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+	mat4 leftLegModel = identity_mat4();
+	leftLegModel = translate(leftLegModel, vec3(0.0f, -0.5f, 0.0f));
+	leftLegModel = scale(leftLegModel, vec3(1.0f, 3.5f, 1.0f));
+	leftLegModel = leftBottomModel * leftLegModel;
+
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, leftLegModel.m);
+	glUniform3fv(color, 1, bodyColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
+	mat4 rightBottomModel = identity_mat4();
+	rightBottomModel = rotate_x_deg(rightBottomModel, 20 * sin(walk));
+	rightBottomModel = translate(rightBottomModel, vec3(-0.5f, -2.5f, 0.0f));
+	rightBottomModel = scale(rightBottomModel, vec3(0.3f, 0.2f, 0.3f));
+	rightBottomModel = humanModel * rightBottomModel;
+
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, rightBottomModel.m);
+	glUniform3fv(color, 1, greenColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+	mat4 rightLegModel = identity_mat4();
+	rightLegModel = translate(rightLegModel, vec3(0.0f, -0.5f, 0.0f));
+	rightLegModel = scale(rightLegModel, vec3(1.0f, 3.5f, 1.0f));
+	rightLegModel = rightBottomModel * rightLegModel;
+
+	glUniformMatrix4fv(cube_matrix_location, 1, GL_FALSE, rightLegModel.m);
+	glUniform3fv(color, 1, bodyColor);
+	glBindBuffer(GL_ARRAY_BUFFER, human->VBO);
+	human->linkCurrentBuffertoShader(cubeShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+
+
+	glUseProgram(textureShader->ID);
+	//Declare your uniform variables that will be used in your shader
+	int texture_matrix_location = glGetUniformLocation(textureShader->ID, "model");
+	int texture_view_mat_location = glGetUniformLocation(textureShader->ID, "view");
+	int texture_proj_mat_location = glGetUniformLocation(textureShader->ID, "proj");
+
+
+	// Set up the child matrix
+	mat4 modelGround = identity_mat4();
+	modelGround = translate(modelGround, vec3(0.0f, -0.19f, 0.0f));
+	modelGround = scale(modelGround, vec3(100.0f, 10.0f, 100.0f));
+	modelGround = translate(modelGround, vec3(transform_x, 0.0f, transform_y));
+	 
+	// Apply the root matrix to the child matrix
+	modelGround = model * modelGround;
+	// Update the appropriate uniform and draw the mesh again
+	glUniformMatrix4fv(texture_proj_mat_location, 1, GL_FALSE, persp_proj.m);
+	glUniformMatrix4fv(texture_view_mat_location, 1, GL_FALSE, view.m);
+	glUniformMatrix4fv(texture_matrix_location, 1, GL_FALSE, modelGround.m);
+	groundMesh->linkCurrentBuffertoShader(textureShader->ID);
+	grassTexture->Bind(GL_TEXTURE0);
+	glDrawArrays(GL_TRIANGLES, 0, groundMesh->mesh_data.mPointCount);
+	
+	
+	mat4 modelBarrier = identity_mat4();
+	modelBarrier = scale(modelBarrier, vec3(0.01f, 0.1f, 0.01f));
+	modelBarrier = scale(modelBarrier, vec3(2.0f, 2.0f, 2.0f));
+	modelBarrier = translate(modelBarrier, vec3(0.0f, 1.0f, 0.3f));
+
+	modelBarrier = modelGround * modelBarrier;
+
+	// Update the appropriate uniform and draw the mesh again
+	glUniformMatrix4fv(texture_matrix_location, 1, GL_FALSE, modelBarrier.m);
+	houseTexture->Bind(GL_TEXTURE0);
+	houseMesh->linkCurrentBuffertoShader(textureShader->ID);
+	glDrawArrays(GL_TRIANGLES, 0, houseMesh->mesh_data.mPointCount);
+
+
+	//enemy_y = enemy_y - 0.01;
+	mat4 modelEnemies = identity_mat4();
+	modelEnemies = translate(modelEnemies, vec3(enemy_x, 0.0f, enemy_y));
+	modelEnemies = scale(modelEnemies, vec3(0.01f, 0.1f, 0.01f));
+	modelEnemies = scale(modelEnemies, vec3(0.25f, 0.25f, 0.25f));
+	modelEnemies = translate(modelEnemies, vec3(0.0f, 0.935f, 0.0f));
+	modelEnemies = modelGround * modelEnemies;
+	glUniformMatrix4fv(texture_matrix_location, 1, GL_FALSE, modelEnemies.m);
+	if (enemystate == 0) {
+		red->Bind(GL_TEXTURE0);
+		humanMesh->linkCurrentBuffertoShader(meshShader->ID);
+		glDrawArrays(GL_TRIANGLES, 0, humanMesh->mesh_data.mPointCount);
+	}
+
+
+	modelEnemies = identity_mat4();
+	if (enemy_y2 > -30) {
+		enemy_y2 = enemy_y2 - 0.01;
+	}
+	modelEnemies = translate(modelEnemies, vec3(enemy_x2, 0.0f, enemy_y2));
+	modelEnemies = scale(modelEnemies, vec3(0.01f, 0.1f, 0.01f));
+	modelEnemies = scale(modelEnemies, vec3(0.25f, 0.25f, 0.25f));
+	modelEnemies = translate(modelEnemies, vec3(0.0f, 0.935f, 0.0f));
+	modelEnemies = modelGround * modelEnemies;
+	glUniformMatrix4fv(texture_matrix_location, 1, GL_FALSE, modelEnemies.m);
+	if (enemystate2 == 0) {
+		red->Bind(GL_TEXTURE0);
+		humanMesh->linkCurrentBuffertoShader(meshShader->ID);
+		glDrawArrays(GL_TRIANGLES, 0, humanMesh->mesh_data.mPointCount);
+	}
 
 
 
@@ -321,12 +425,12 @@ void init()
 	humanMesh->generateObjectBufferMesh("../models/human.dae");
 	aim = new Aim();
 	aim->generateObjectBuffer();
-	ground = new Mesh();
-	ground->generateObjectBufferMesh("../models/ground.dae");
-	house = new Mesh();
-	house->generateObjectBufferMesh("../models/house.dae");
-	test = new TextureScreen();
-	test->generateObjectBuffer();
+	groundMesh = new Mesh();
+	groundMesh->generateObjectBufferMesh("../models/ground.dae");
+	houseMesh = new Mesh();
+	houseMesh->generateObjectBufferMesh("../models/house.dae");
+	human = new TextureScreen();
+	human->generateObjectBuffer();
 
 	// load tecture
 	grassTexture = new Texture(GL_TEXTURE_2D, "../textures/grass.JPG");
@@ -338,8 +442,6 @@ void init()
 	red = new Texture(GL_TEXTURE_2D, "../textures/red.JPG");
 	red->Load();
 
-	//sight = new Texture(GL_TEXTURE_2D, "../textures/test.png");
-	//sight->Load();
 	
 }
 
@@ -352,7 +454,8 @@ void keypress(unsigned char key, int x, int y) {
 			transform_y = transform_y + 0.1f * cos(rotate_x*PI / 180.0f);
 			transform_x = transform_x - 0.1f * sin(rotate_x*PI / 180.0f);
 		}
-
+		walk = walk + 0.1f;
+		printf("walk: %f \n", sin(walk));
 		printf("Transform: x  %f  y %f\n", transform_x, transform_y);
 	}
 	if (key == 'a') {
@@ -379,16 +482,6 @@ void keypress(unsigned char key, int x, int y) {
 		}
 	}
 
-	if (key == 'e') {
-		if (shoot()) {
-			enemystate = 1;
-		}
-
-		if (shoot2()) {
-			enemystate2 = 1;
-		}
-		
-	}
 
 	glutPostRedisplay();
 }
@@ -396,11 +489,13 @@ void keypress(unsigned char key, int x, int y) {
 
 void mousepress(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		if (viewstate == 0) {
-			viewstate = 1;
-		}
-		else if (viewstate == 1) {
-			viewstate = 0;
+		if (viewstate == 1) {
+			if (shoot()) {
+				enemystate = 1;
+			}
+			if (shoot2()) {
+				enemystate2 = 1;
+			}
 		}
 	}
 
@@ -411,6 +506,8 @@ void mousepress(int button, int state, int x, int y) {
 			movemouse = 1;
 		} else {
 			movemouse = 0;
+			view_rotate_x = 0;
+			view_rotate_y = 0;
 		}
 		
 	}
